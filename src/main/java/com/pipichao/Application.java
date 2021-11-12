fca9c2728c6d2ab537fa48ece1d29ab1bb84743e
wangchao <wangchao@zhiyuanhui.com>
2021-11-12 14:00:22 +0800

    Initial commit

---
+++ src/main/java/com/pipichao/Application.java
@@ -0,0 +1,17 @@
+package com.pipichao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author wangchao
+ * @Date 2021/10/9
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
